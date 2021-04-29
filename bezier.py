be30fefb8d817ead6435923842bc09a00ebc4b41
Jens Zamanian <jens.zamanian@gmail.com>
2021-04-29 23:50:55 +0200

    Finish offset_curve.
    The curve offset now works decently. There are some improvments to the
    functionality possible. There is also a lot of improvments to the code
    that can be done.

--- bezier.py
+++ bezier.py
@@ -87,7 +87,7 @@ def is_close(a, b, threshold = 0.01):
     comps = all(math.isclose(*c, abs_tol = threshold) for c in zip(a,b))
     return comps
 
-def is_colinear(v1, v2, threshold = 0.01):
+def is_colinear(v1, v2, threshold = 0.00000001):
     return v1.cross(v2).length < threshold
 
 
@@ -95,10 +95,15 @@ def is_colinear(v1, v2, threshold = 0.01):
 
 def bezier_from_Blender(name):
     cu = bpy.data.collections['Collection'].objects[name]
-    p0 = cu.data.splines[0].bezier_points[0].co
-    p1 = cu.data.splines[0].bezier_points[0].handle_right
-    p2 = cu.data.splines[0].bezier_points[1].handle_left
-    p3 = cu.data.splines[0].bezier_points[1].co
+    # We need to translate with the object position to get the global 
+    # positions. 
+    # A better way would probably be to get the origin of current curve
+    # and instead translate all objects to that origin. 
+    location = cu.location
+    p0 = cu.data.splines[0].bezier_points[0].co + location
+    p1 = cu.data.splines[0].bezier_points[0].handle_right + location
+    p2 = cu.data.splines[0].bezier_points[1].handle_left + location
+    p3 = cu.data.splines[0].bezier_points[1].co + location
     return Bezier(p0, p1, p2, p3)
 
 ### End: Utility Functions ###
@@ -183,6 +188,9 @@ class Bezier():
             return self.bounding_box
         elif attribute == 'extrema':
             self.extrema = self._get_extrema()
+            return self.extrema
+        elif attribute == 'absolute_extrema':
+            self.extrema = self._get_extrema(absolute=True)
             return self.extrema
         elif attribute == 'inflection_points':
             self.inflection_points = self._get_inflection_points()
@@ -274,7 +282,7 @@ class Bezier():
             aligned_points.append(m @ p)
         return aligned_points
 
-    def _get_bounding_box(self):
+    def _get_bounding_box(self, tight=False):
         """
         Calculate the bounding box of the curve. 
         """
@@ -282,23 +290,34 @@ class Bezier():
         # is deemed useful. 
         # TODO: Consider returning the box in some other format. 
         # TODO: Consider using a Rectangle class for this. 
-        extrema = self.extrema
+        extrema = self._get_extrema(absolute=tight) # TODO: Does not work! 
+        # Need to calculate the rotation and translation also! 
         min_x = self.__call__(extrema[0])[0]
         max_x = self.__call__(extrema[1])[0]
         min_y = self.__call__(extrema[2])[1]
         max_y = self.__call__(extrema[3])[1]
         return (min_x, max_x, min_y, max_y)
 
-    def _get_extrema(self):
+    def _get_extrema(self, absolute=False):
         """
         Returns the parameter values for the minimum and maximum of the curve
         in the x and y coordinate. 
         (tx_min, tx_min, ty_max, ty_max)
         """
-        p0 = self.points[0]
-        p1 = self.points[1]
-        p2 = self.points[2]
-        p3 = self.points[3]
+        # TODO: Change so that this is the absolute extrema. 
+        # This can be done by:
+        # 1. Translate so that p0 = (0,0)
+        # 2. Rotate so that p3 on the positive y_axis. 
+        # 3. The new curve max_x and min_x are now the global maximum values. 
+        if absolute:
+            p0, p1, p2, p3 = self.aligned
+        else:
+            p0, p1, p2, p3 = self.points
+
+        # p0 = self.points[0]
+        # p1 = self.points[1]
+        # p2 = self.points[2]
+        # p3 = self.points[3]
 
         a = (3 * (-p0 + 3 * p1 - 3 * p2 + p3))
         b = 6 * (p0 - 2*p1 + p2)
@@ -349,20 +368,68 @@ class Bezier():
         Splits the curve at each extrema and each inflection point
         and returns a list of these. 
         """
+        abs_extrema = self.absolute_extrema
+        print('abs', abs_extrema)
         extrema = self.extrema
+        print('ext', extrema)
         inflections = self.inflection_points
-        total = list(set(extrema + inflections)) # Remove any doubles. 
+        total = list(set(abs_extrema + extrema + inflections)) # Remove any doubles. 
         total.sort() 
-        if 0 not in total:
+        # total = [i for i in total if i > 0.05 and i < 0.95]
+        if 0.0 not in total:
             total.insert(0, 0.0)
-        if 1 not in total:
-            total.append(1)
+        if 1.0 not in total:
+            total.append(1.0)
 
         curves = []
         for i in range(len(total)-1):
             curves.append(self.split(total[i], total[i+1]))
 
         return curves
+
+    def _get_simplified(self):
+        """Splits all reduced curves down the middle. Keeps splitting 
+        until none of the curves has the middle of the curve too far
+        from the center of the box created by the points. 
+        """
+
+        beziers = []
+        for bezier in self.reduced:
+            beziers += bezier.split(0.5)
+
+        all_simple = False
+        
+        while not all_simple:
+            all_simple = True
+            new_set = []
+            for bez in beziers:
+                if bez._is_good() < 0.03:
+                    new_set.append(bez)
+                else:
+                    all_simple = False
+                    new_set += bez.split(0.5)
+            beziers = new_set
+
+        return beziers
+
+
+
+    def _is_good(self):
+        mid = self.__call__(0.5)
+        mid.resize_2d()
+        p = self.points
+        a = (p[3] + p[0])/2
+        b = (p[2] + p[1])/2 
+        c = (p[3] + p[2])/2
+        d = (p[1] + p[0])/2
+        a.resize_2d()
+        b.resize_2d()
+        c.resize_2d()
+        d.resize_2d()
+        ints = mathutils.geometry.intersect_line_line_2d(a, b, c, d)
+        jam = max((a-b).length, (c-d).length)
+        return (ints-mid).length / jam
+        
 
     def is_simple(self):
         """
@@ -458,6 +525,7 @@ class Bezier():
         cu = bpy.data.curves.new(self.name, 'CURVE')
         ob = bpy.data.objects.new(self.name, cu)
         bpy.data.collections['Collection'].objects.link(ob)
+        ob.data.resolution_u = 64
         cu.splines.new('BEZIER')
         spline = cu.splines[-1]
         
@@ -472,6 +540,7 @@ class Bezier():
         """
         Splits the curve up in simple parts
         """
+        # TODO: Is this needed? I already have _get_reduced. 
         results = [] 
         extrema = self.extrema() 
 
@@ -531,17 +600,36 @@ class Bezier():
         """
         left_offset = []
         right_offset = []
-        m1 = Matrix().Rotation(math.pi/2, 3, 'Z')
-        m2 = Matrix().Rotation(-math.pi/2, 3, 'Z')
-        first = True # For the first curve we need to create the first point.
-        test = True
-        beziers = []
+        m = Matrix().Rotation(-math.pi/2, 3, 'Z') # TODO: Move to constants. 
+        first = True # For the first curve we need to create the first point. (Blender related?)
+        beziers = self._get_simplified()
 
-        # TODO: Fix linear curves. 
-        for bezier in self.reduced:
-            beziers += bezier.split(0.5)
-        
-        beziers = [self]
+
+        # TODO: All this should be moved to reduced. 
+        # for bezier in self.reduced:
+        #     beziers += bezier.split(0.5)
+        # new_set = []
+        # for bez in beziers:
+        #     if bez._is_good() < 0.05:
+        #         new_set.append(bez)
+        #     else:
+        #         new_set += bez.split(0.5)
+
+
+        # beziers = new_set
+
+        # new_set = []
+        # for bez in beziers:
+        #     if bez._is_good() < 0.05:
+        #         new_set.append(bez)
+        #     else:
+        #         new_set += bez.split(0.5)
+
+        # beziers = new_set
+        # for bez in beziers:
+        #     print('is g', bez._is_good())
+
+        # beziers = [self]
         # TODO: Consider if there is some smarter way to do this loop. 
         # In case the curve was not suitable for offsetting, 
         # we split again and try again. 
@@ -549,20 +637,24 @@ class Bezier():
         # in advance and simply add that to Bezier.reduced so that we are
         # sure offsettinga will work. 
         i = 0
-        while i < len(beziers):
+        j = 0
+        while i < len(beziers) and j < 100: #max iterations
+            j += 1
             bezier = beziers[i]
             p = bezier.points
             n0 = bezier.normal(0) # Normal at t = 0
             n1 = bezier.normal(1) # Normal at t = 1 
+
             # TODO: How do we handle a straight line? 
-            if bezier.is_clockwise(): 
-                m = m1
-                l = 1
-            else:
-                m = m2
-                l = -1 
-            nn = l * m @ (p[2] - p[1])
-            nn.normalize()
+            # if bezier.is_clockwise(): 
+            #     m = m2
+            #     # l = 1
+            # else:
+            #     m = m2
+            #     # l = -1 
+
+            nm = m @ (p[2] - p[1])
+            nm.normalize()
             
             # Left offset 
             # Reuse the point of the last point. They should be the same. 
@@ -572,9 +664,9 @@ class Bezier():
                 a0 = p[0] - n0 * d
 
             a1p = p[1] - n0 * d 
-            a1pp = p[1] + nn * d
+            a1mp = p[1] - nm * d
             a2p = p[2] - n1 * d
-            a2pp = p[2] + nn * d
+            a2mp = p[2] - nm * d
             a3 = p[3] - n1 * d
 
             # Problematic cases:
@@ -585,24 +677,41 @@ class Bezier():
             # 1. Handles coincide. In this case we get nn = (0,0,0). 
             # 2. and 3. We need to do something smart here. 
 
-            print('nn', nn)
-            print('p2', p[2])
-            print('a3', a3)
-            print('a2p', a2p)
-            print('a1pp', a1pp)
-            print('a2pp', a2pp)
+            # print('nm', nm)
+            # print('p2', p[2])
+            # print('a3', a3)
+            # print('a2p', a2p)
+            # print('a1mp', a1mp)
+            # print('a2mp', a2mp)
+
             # TODO: Write own functions for this.
             # In case the lines are colinear, then we should
-            # use a1 = a1pp
-            # a2 = a2pp 
+            # use a1 = a1mp
+            # a2 = a2mp 
             # I think. 
-            a1i = mathutils.geometry.intersect_line_line(a0, a1p, a1pp, a2pp)
-            a2i = mathutils.geometry.intersect_line_line(a3, a2p, a1pp, a2pp)
-            if a1i is None or a2i is None:
-                beziers[i:i+1] = bezier.split(0.5)
-                continue
-            a1 = a1i[0]
-            a2 = a2i[0]
+            a1 = None
+            a2 = None
+            if is_colinear(a1p - a0, a2mp - a1mp):
+                a1 = a1mp
+            if is_colinear(a2mp - a1mp, a2p - a3):
+                a2 = a2mp
+
+            # TODO: Reduce a line here.
+            if a1 is None:
+                a1i = mathutils.geometry.intersect_line_line(a0, a1p, a1mp, a2mp)
+                a1 = a1i[0]
+            if a2 is None: 
+                a2i = mathutils.geometry.intersect_line_line(a3, a2p, a1mp, a2mp)
+                a2 = a2i[0]
+
+            # TODO: Probably this is no longer needed. 
+            # Since we check for colinearity we remove the possibility
+            # of no solutions.
+            # if a1i is None or a2i is None:
+            #     beziers[i:i+1] = bezier.split(0.5)
+                # continue
+            # a1 = a1i[0]
+            # a2 = a2i[0]
             
             # Right offset
             # Reuse the point of the last point. They should be the same. 
@@ -611,18 +720,34 @@ class Bezier():
             else:
                 b0 = p[0] + n0 * d
             b1p = p[1] + n0 * d 
-            b1pp = p[1] - nn * d
+            b1mp = p[1] + nm * d
             b2p = p[2] + n1 * d
-            b2pp = p[2] - nn * d
+            b2mp = p[2] + nm * d
             b3 = p[3] + n1 * d
 
-            b1i = mathutils.geometry.intersect_line_line(b0, b1p, b1pp, b2pp)
-            b2i = mathutils.geometry.intersect_line_line(b3, b2p, b1pp, b2pp)
-            if b1i is None or b2i is None:
-                beziers[i:i+1] = bezier.split(0.5)
-                continue
-            b1 = b1i[0]
-            b2 = b2i[0]
+            b1 = None
+            b2 = None
+
+            if is_colinear(b1p - b0, b2mp - b1mp):
+                b1 = b1mp
+            if is_colinear(b2mp - b1mp, b2p - b3):
+                b2 = b2mp
+
+            if b1 is None:
+                b1i = mathutils.geometry.intersect_line_line(b0, b1p, b1mp, b2mp)
+                b1 = b1i[0]
+            if b2 is None: 
+                b2i = mathutils.geometry.intersect_line_line(b3, b2p, b1mp, b2mp)
+                b2 = b2i[0]
+
+            # TODO: Probably this is no longer needed. 
+            # Since we check for colinearity we remove the possibility
+            # of no solutions.
+            # if b1i is None or b2i is None:
+            #     beziers[i:i+1] = bezier.split(0.5)
+            #     continue
+            # b1 = b1i[0]
+            # b2 = b2i[0]
 
             a = Bezier(a0, a1, a2, a3)
             b = Bezier(b0, b1, b2, b3)
@@ -637,12 +762,20 @@ class Bezier():
         self.left_offset.add_to_Blender()
         self.right_offset.add_to_Blender()
 
+
     def is_clockwise(self):
+        # TODO: Perhaps this function is not needed! 
         # TODO: Handle straight lines? 
+        # TODO: TESTING WITH JUST RETURNING TRUE. 
+        # I do not think it matter if there is a straight line. 
         a = self.points[1] - self.points[0]
         a.resize_2d()
         b = self.points[3] - self.points[0]
         b.resize_2d()
+        if a.length == 0.0 or b.length == 0.0:
+            return False
+        # if a.length == 0.0 or b.length == 0.0:
+        #     return True
         if a.angle_signed(b) >= 0:
             return True
         else:
@@ -721,6 +854,7 @@ class Curve():
         # TODO: How should we choose which collection to add the curve to? 
         cu = bpy.data.curves.new(self.name, 'CURVE')
         ob = bpy.data.objects.new(self.name, cu)
+        ob.data.resolution_u = 64
         bpy.data.collections['Collection'].objects.link(ob)
         cu.splines.new('BEZIER')
         spline = cu.splines[-1]
@@ -736,7 +870,7 @@ class Curve():
             spline.bezier_points[-1].co = bez.points[3]
             spline.bezier_points[-1].handle_left = bez.points[2]
 
-    def intersections(self, threshold = 0.01):
+    def intersections(self, threshold = 0.001):
         """
         Find the intersections within the curve. 
         The result is a dict e.g. {2 : [(t, t'), ...], (3, 4): [(...)]}
